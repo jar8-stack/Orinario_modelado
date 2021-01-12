9eb4719b4c83eb471d5cd55659e49cf8bf17e6a7
jar8-stack <bioshock450226@gotmail.com>
2021-01-12 03:53:56 -0600

    api vista lista abarrrots

--- stock_listo.php
+++ stock_listo.php
@@ -18,7 +18,27 @@
 		</div>
 	</section>
 
-    </table>
+  <?php
+    $api_url_productos= "http://localhost:3000/Producto";
+
+    $json_data_products= file_get_contents($api_url_productos);
+
+    $response_data_products= json_decode($json_data_products);
+
+    //$product_data= $response_data_products->data;
+
+    foreach ($response_data_products as $product) {
+      echo "<ol>";
+      echo "<li>";
+    	echo "".$product->Descripcion;
+      echo "</li>";
+    	echo "<li>";
+    	echo "".$product->Precio;
+    	echo "</li>";
+      echo "</ol>";
+    }
+
+   ?>
 </body>
 
 </html>
