16dbe5ff7e33de5cf9dfa92d275de34e549ffbdd
Carlos Ivan Aguero <aguero_5209@hotmail.com>
2021-01-12 02:51:13 -0600

    abarrotes

--- stock_listo.php
+++ stock_listo.php
@@ -1,10 +1,38 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Stock</title>
-  </head>
-  <body>
 
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>Stock</title>
+</head>
+
+<body>
+  <header id="header">
+    <div class="logo"><a href="index.php">Tiendita + Appl<span> Hanna Lizarraga</span></a></div>
+    <a href="#menu" class="menu">Menu</a>
+    <link rel="stylesheet" href="css/main.css" type="text/css"/>
+  </header>
+
+  <nav id="menu">
+    <ul class="links">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="stock.php">Stock Escenciales</a></li>
+      <li><a href="stock2.php">Stock</a></li>
+      <li><a href="users.php">Iniciar Sesion</a></li>
+      <li><a href="compra.php">Compras</a></li>
+      <li><a href="contactanos.php">Contactános</a></li>
+      <li><a href="dom.php">Proveedores</a></li>
+    </ul>
+  </nav>
+
+  <section id="One" class="wrapper style4">
+		<div class="inner">
+			<header class="align-center">
+				<p>Stock</p>
+				<h2>Abarrotes</h2>
+			</header>
+		</div>
+	</section>
+</body>
+
 </html>
